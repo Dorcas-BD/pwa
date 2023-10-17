1ba6791bc915e6982146626afffe6c55366486c5
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-10-17 10:01:57 +0100

    About component

--- pages/about/page.tsx
+++ pages/about/page.tsx
@@ -2,52 +2,39 @@
 
 import { useState } from "react";
 import "../../styles/styles.css";
-import { type } from "os";
 
-const About = () => {
-  const [activeTab, setActiveTab] = useState("Skills");
+interface Skill {
+  title: string;
+  content: string;
+}
+
+interface Experience {
+  title: string;
+  content: string;
+}
+
+interface Education {
+  title: string;
+  content: string;
+}
+
+interface Tab {
+  title: string;
+  content: Skill[] | Experience[] | Education[];
+}
+
+const About: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("Skills");
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
 
-  type Tab = {
-    title: string;
-    content: { tabTitle: string; tabContent: string };
-  };
-
-  const tabs: Tab[] = [
-    {
-      title: "skills",
-      content: {
-        tabTitle: "Technical Skills",
-        tabContent:
-          "HTML, CSS, SCSS/SASS, Javascript, ReactJs, Material UI, Git, Blockchain, Web3.js, Solidity, Hardhat, and Truffle.",
-      },
-    },
-    {
-      title: "Experience",
-      content: {
-        tabTitle: "Technical Skills",
-        tabContent:
-          "HTML, CSS, SCSS/SASS, Javascript, ReactJs, Material UI, Git, Blockchain, Web3.js, Solidity, Hardhat, and Truffle.",
-      },
-    },
-    {
-      title: "skills",
-      content: {
-        tabTitle: "Technical Skills",
-        tabContent:
-          "HTML, CSS, SCSS/SASS, Javascript, ReactJs, Material UI, Git, Blockchain, Web3.js, Solidity, Hardhat, and Truffle.",
-      },
-    },
-  ];
-
-  const skills = [
+  const skills: Skill[] = [
     {
       title: "Technical Skills",
       content:
-        "HTML, CSS, SCSS/SASS, Javascript, ReactJs, Material UI, Git, Blockchain, Web3.js, Solidity, Hardhat, and Truffle.",
+        "HTML, CSS, SCSS/SASS, Javascript, ReactJs, Material UI, Git, Blockchain, Web3.js, Solidity, Hardhat, and Truffle",
     },
     {
       title: "Soft Skills",
@@ -56,7 +43,7 @@ const About = () => {
     },
   ];
 
-  const experience = [
+  const experiences: Experience[] = [
     {
       title: "Frontend Developer Intern",
       content: "Health Information System - July 2021 - December, 2021",
@@ -71,19 +58,18 @@ const About = () => {
     },
   ];
 
-  const education = [
+  const educations: Education[] = [
     {
       title: " BSc Computer Science with Mathematics ",
       content: " Obafemi Awolowo University - April 2017 - August 2023",
     },
   ];
 
-  //   const tabs = [
-  //     { title: "Skills", content: skills },
-  //     { title: "Experience", content: experience },
-  //     { title: "Education", content: education },
-  //   ];
-
+  const tabs: Tab[] = [
+    { title: "Skills", content: skills },
+    { title: "Experiences", content: experiences },
+    { title: "Education", content: educations },
+  ];
   return (
     <section className="about section" id="about">
       <div className="about-container">
@@ -124,7 +110,7 @@ const About = () => {
             <ul>
               {tabs
                 .find((tab) => tab.title === activeTab)
-                .content.map((item, i) => (
+                ?.content.map((item, i) => (
                   <li key={i}>
                     <span>{item.title}</span>
                     <br />
