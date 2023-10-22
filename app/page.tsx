f3d46ccf8164a15d5e8bcb55193d436579ca57ef
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-10-22 07:26:03 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@/pages/footer/page";
 import Hero from "@/pages/hero/page";
 import NavBar from "@/pages/navbar/page";
 import Projects from "@/pages/projects/page";
-import Image from "next/image";
 
 export default function Home() {
   return (
